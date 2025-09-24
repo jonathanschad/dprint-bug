5656384e82bb970d5f73960abb11418f25727b18
Jonathan Schad <jonathan.schad@pickware.de>
2025-09-24 18:04:46 +0200

    Reproduce bug in minimal setup

---
+++ main.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import copyButton from '@/components/buttons/copy-button.vue';
+
+const longerName = {
+    alsoQuiteALongName: 'value',
+};
+</script>
+
+<template>
+  <div>
+    <div>
+      <div>
+        <div>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <div>
+                      <td>
+                        <code class="value-uuid">{{
+                          longerName.alsoQuiteALongName
+                        }}</code>
+                        <copy-button
+                          v-if="longerName.alsoQuiteALongName"
+                          :value="longerName.alsoQuiteALongName"
+                        />
+                      </td>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
