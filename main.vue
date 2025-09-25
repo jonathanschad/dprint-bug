eaa7a47600f33b928bee6b3e3a947e5f500fa76f
Jonathan Schad <jonathan.schad@pickware.de>
2025-09-25 10:45:59 +0200

    Reduce to minimal reproducable setup

--- main.vue
+++ main.vue
@@ -1,39 +1,34 @@
 <script setup lang="ts">
-import copyButton from '@/components/buttons/copy-button.vue';
-
 const longerName = {
-    alsoQuiteALongName: 'value',
+    alsoQuiteALongName: "value",
 };
 </script>
 
 <template>
-  <div>
     <div>
-      <div>
         <div>
-          <div>
             <div>
-              <div>
                 <div>
-                  <div>
                     <div>
-                      <td>
-                        <code class="value-uuid">{{
-                          longerName.alsoQuiteALongName
-                        }}</code>
-                        <copy-button
-                          v-if="longerName.alsoQuiteALongName"
-                          :value="longerName.alsoQuiteALongName"
-                        />
-                      </td>
+                        <div>
+                            <div>
+                                <div>
+                                    <div>
+                                        <div>
+                                            <td>
+                                                <div
+                                                    v-if="longerName.alsoQuiteALongName"
+                                                    :value="longerName.alsoQuiteALongName"
+                                                />
+                                            </td>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
